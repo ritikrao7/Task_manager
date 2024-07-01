e3e0ceb35649bcaa3480dc3922e9d3a52110daac
Ritik <ritikraobamboriya@gmail.com>
2024-07-01 15:41:34 +0530

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,32 @@
+import Filterbar from './Components/Filterbar';
+
+import Navbar from './Components/Navbar';
+
+import { TaskProvider } from './Context/TaskContext';
+
+import Tasks from "./Components/TaskList"
+ 
+
+function App() {
+
+    return (
+
+        <>
+
+            <TaskProvider>
+
+                <Navbar />
+
+                <Filterbar />
+
+                <Tasks />
+
+            </TaskProvider>
+
+        </>
+
+    );
+}
+ 
+
+export default App;
